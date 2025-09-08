f5b854be5d0691828836b25e7baefa08d522fc30
Matheus-Matta <matheuseduardo3004@gmail.com>
2025-09-07 22:21:42 -0300

    fix conversation duplicate evolution incomming

--- app/services/evolution/conversation_ensurer.rb
+++ app/services/evolution/conversation_ensurer.rb
@@ -5,30 +5,29 @@ module Evolution
     module_function
 
     def ensure!(inbox:, contact_inbox:)
-      if inbox.lock_to_single_conversation?
-        conv = ::Conversation.where(contact_inbox_id: contact_inbox.id)
+      ActiveRecord::Base.transaction(requires_new: true) do
+        ::ContactInbox.lock.where(id: contact_inbox.id).pluck(:id)
+        if inbox.lock_to_single_conversation?
+          conv = ::Conversation.where(contact_inbox_id: contact_inbox.id)
+                               .order(updated_at: :desc).first
+          return conv if conv.present?
+        end
+        conv = ::Conversation.where(contact_inbox_id: contact_inbox.id, status: :open)
                              .order(updated_at: :desc).first
         return conv if conv.present?
+        last_conv = ::Conversation.where(contact_inbox_id: contact_inbox.id)
+                                  .order(updated_at: :desc).first
+        if last_conv&.resolved? && last_conv.updated_at >= 48.hours.ago
+          last_conv.open!
+          return last_conv
+        end
+        permitted = ActionController::Parameters.new(
+          account_id:            inbox.account_id,
+          inbox_id:              inbox.id,
+          additional_attributes: {}
+        ).permit!
+        ::ConversationBuilder.new(contact_inbox:, params: permitted).perform
       end
-
-      conv = ::Conversation.where(contact_inbox_id: contact_inbox.id, status: :open)
-                           .order(updated_at: :desc).first
-      return conv if conv.present?
-
-      last_conv = ::Conversation.where(contact_inbox_id: contact_inbox.id)
-                                .order(updated_at: :desc).first
-      if last_conv&.resolved? && last_conv.updated_at >= 48.hours.ago
-        last_conv.open!
-        return last_conv
-      end
-
-      permitted = ActionController::Parameters.new(
-        account_id:            inbox.account_id,
-        inbox_id:              inbox.id,
-        additional_attributes: {}
-      ).permit!
-
-      ::ConversationBuilder.new(contact_inbox:, params: permitted).perform
     end
   end
 end
