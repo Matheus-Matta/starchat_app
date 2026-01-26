5b43a25e359bdb44d668cc7c45cca0b29a227947
Matheus-Matta <matheuseduardo3004@gmail.com>
2026-01-26 15:08:29 -0300

    style: reorder ActiveRecord callbacks to statisfy rubocop

--- app/models/channel/whatsapp.rb
+++ app/models/channel/whatsapp.rb
@@ -32,8 +32,8 @@ class Channel::Whatsapp < ApplicationRecord
   validates :phone_number, presence: true, uniqueness: true
   validate :validate_provider_config
 
-  before_destroy :teardown_webhooks
   after_create :sync_templates
+  before_destroy :teardown_webhooks
   after_commit :setup_webhooks, on: :create, if: :should_auto_setup_webhooks?
 
   def name
