d2878ec5c042422bcd6da560a37bc5e7601584bc
Matheus-Matta <matheuseduardo3004@gmail.com>
2026-01-26 15:18:22 -0300

    chore: disable inconsistent rubocop rule for callbacks

--- app/models/channel/whatsapp.rb
+++ app/models/channel/whatsapp.rb
@@ -32,9 +32,11 @@ class Channel::Whatsapp < ApplicationRecord
   validates :phone_number, presence: true, uniqueness: true
   validate :validate_provider_config
 
+  # rubocop:disable Rails/ActiveRecordCallbacksOrder
   after_create :sync_templates
   before_destroy :teardown_webhooks
   after_commit :setup_webhooks, on: :create, if: :should_auto_setup_webhooks?
+  # rubocop:enable Rails/ActiveRecordCallbacksOrder
 
   def name
     'Whatsapp'
