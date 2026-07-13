25e67c8b1960e3601fac0f127c2ccbaa8de32959
Matheus-Matta <matheuseduardo3004@gmail.com>
2026-07-13 15:05:16 -0300

    fix(super-admin): restore account creation and select all features

--- app/javascript/entrypoints/superadmin.js
+++ app/javascript/entrypoints/superadmin.js
@@ -1,1 +1,18 @@
 import '../dashboard/assets/scss/super_admin/index.scss';
+
+const selectAllAccountFeatures = event => {
+  const container = event.currentTarget.closest('[data-account-features]');
+  const uncheckedFeatures = Array.from(
+    container.querySelectorAll('input[type="checkbox"]:not(:checked)')
+  );
+
+  uncheckedFeatures.forEach(checkbox => checkbox.click());
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  document
+    .querySelectorAll('[data-select-all-account-features]')
+    .forEach(button =>
+      button.addEventListener('click', selectAllAccountFeatures)
+    );
+});
--- starchat/app/controllers/starchat/super_admin/accounts_controller.rb
+++ starchat/app/controllers/starchat/super_admin/accounts_controller.rb
@@ -1,13 +1,31 @@
 module Starchat::SuperAdmin::AccountsController
-  def update
-    if params[:account] && params[:account][:manually_managed_features].present?
-      service = ::Internal::Accounts::InternalAttributesService.new(requested_resource)
-      service.manually_managed_features = params[:account][:manually_managed_features]
+  def create
+    manually_managed_features = extract_manually_managed_features
 
-      # Remove the manually_managed_features from params to prevent ActiveModel::UnknownAttributeError
-      params[:account].delete(:manually_managed_features)
+    super do |account|
+      update_manually_managed_features(account, manually_managed_features)
     end
+  end
+
+  def update
+    manually_managed_features = extract_manually_managed_features
+    update_manually_managed_features(requested_resource, manually_managed_features)
 
     super
+  end
+
+  private
+
+  def extract_manually_managed_features
+    return unless params[:account] && params[:account][:manually_managed_features].present?
+
+    params[:account].delete(:manually_managed_features)
+  end
+
+  def update_manually_managed_features(account, features)
+    return if features.blank?
+
+    service = ::Internal::Accounts::InternalAttributesService.new(account)
+    service.manually_managed_features = features
   end
 end
